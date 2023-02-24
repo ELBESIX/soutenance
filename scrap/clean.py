39633980f33dcdd48d5900eac5dc6bfc22b3f7d7
ELBESIX <123724589+ELBESIX@users.noreply.github.com>
2023-02-24 03:24:29 +0100

    Add files via upload

---
+++ scrap/clean.py
@@ -0,0 +1,25 @@
+import pandas as pd
+import numpy as np
+import re
+
+# chargement et affichage des données
+data = pd.read_csv('lol.csv')
+print(data)
+
+
+def tests(data,columns):
+    print(data.dtypes)
+    print(data.isnull().sum())
+    data_null = data.isnull().sum()
+    print(data_null[data_null>0])
+    print(data.loc[data[columns].duplicated(keep=False),:])
+tests(data,["name"])
+
+def duplicate(data,columns):
+    data.drop_duplicates(subset=columns, inplace=True, ignore_index=True)
+
+def clean(data,columns):
+    duplicate(data,columns)
+    data.to_csv("cleaned_links.csv", index=False)
+
+# clean(data,["name"])
